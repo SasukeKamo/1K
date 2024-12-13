90afcad7f476ff18d1de838defc432266cfe0a0b
paszym <parol.szymon@protonmail.com>
2024-12-13 22:58:12 +0100

    waiting for dealing cards panel added to local single player mode

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -861,9 +861,9 @@ public class GameManager : MonoBehaviourPunCallbacks
             card.gameObject.transform.SetParent(t.transform);
         }
 
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(2.5f);
 
-                // move cards from table to winner's hand
+        // move cards from table to winner's hand
         Card[] trickCards = trickManager.GetTrickCards();
 
         for (int i = 0; i < 4; i++)
@@ -875,7 +875,7 @@ public class GameManager : MonoBehaviourPunCallbacks
         trickManager.ClearTrickCards();
         InputHandler.Instance.sortingOrder = 1;
         UpdateCardVisibility();
-
+        waitingForCardDialog.SetActive(true);
 
         // handle dealing cards to other players
         List<Card> hand = GetPlayerHand(currentPlayer);
@@ -899,6 +899,7 @@ public class GameManager : MonoBehaviourPunCallbacks
             yield return new WaitForSeconds(1.0f);
             InputHandler.Instance.OnClickHandle(card);
         }
+        waitingForCardDialog.SetActive(false);
     }
 
 
